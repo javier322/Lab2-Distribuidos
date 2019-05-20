9e4ce1f8dc50f6974543f1fda4fe52fabcd38d5c
javier <javier.pinto.g@usach.cl>
2019-05-20 00:29:47 -0400

    Modelo clasificador

---
+++ classifier.py
@@ -0,0 +1,117 @@
+
+from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler
+from pyspark.ml import Pipeline, PipelineModel
+from pyspark.ml.classification import LogisticRegression, LogisticRegressionModel
+
+from pyspark.sql.types import StructType,StructField
+from pyspark.sql.types import IntegerType, StringType
+from sparkSession import SPARK_SESSION
+import json
+
+
+SCHEMA = StructType([
+    StructField("age", IntegerType(), True),
+    StructField("job", StringType(), True),
+    StructField("marital", StringType(), True),
+    StructField("education", StringType(), True),
+    StructField("default", StringType(), True),
+    StructField("balance", IntegerType(), True),
+    StructField("housing", StringType(), True),
+    StructField("loan", StringType(), True),
+    StructField("contact", StringType(), True),
+    StructField("day", IntegerType(), True),
+    StructField("month", StringType(), True),
+    StructField("duration", IntegerType(), True),
+    StructField("campaign", IntegerType(), True),
+    StructField("pdays", IntegerType(), True),
+    StructField("previous", IntegerType(), True),
+    StructField("poutcome", StringType(), True),
+    StructField("deposit", StringType(), True)])
+
+
+def makePrediction(data):
+
+    #stringJson=json.dumps(data)
+    jsonStringRdd=SPARK_SESSION.sparkContext.parallelize(data)
+    df=SPARK_SESSION.read.option('multiline', "true").json(jsonStringRdd,SCHEMA)
+
+
+    df.show()
+
+    pipelineModel=PipelineModel.load("PipelineModel")
+
+    df = pipelineModel.transform(df)
+    selectedCols = ['features'] + df.columns
+    df = df.select(selectedCols)
+
+    model=LogisticRegressionModel.load("ModelClassficator")
+    result=model.transform(df)
+    value=result.select('prediction' ).collect()
+    predictions=[]
+
+    for p  in value:
+        e=p.asDict()['prediction']
+        print(e)
+        predictions.append({"prediction":e})
+
+    return predictions
+
+
+
+
+# element=[
+#    {
+#     "age": 23,
+#     "job": "student",
+#     "marital": "single",
+#     "education": "tertiary",
+#     "default": "no",
+#     "balance": 0,
+#     "housing": "no",
+#     "loan": "no",
+#     "contact": "cellular",
+#     "duration": 120,
+#     "campaign": 1,
+#     "pdays": -1,
+#     "previous": 0,
+#     "poutcome": "failure"
+#   },
+#    {
+#     "age": 40,
+#     "job": "student",
+#     "marital": "single",
+#     "education": "tertiary",
+#     "default": "no",
+#     "balance": 0,
+#     "housing": "no",
+#     "loan": "no",
+#     "contact": "cellular",
+#     "duration": 120,
+#     "campaign": 1,
+#     "pdays": -1,
+#     "previous": 0,
+#     "poutcome": "failure"
+#   },
+#    {
+#     "age": 50,
+#     "job": "student",
+#     "marital": "single",
+#     "education": "tertiary",
+#     "default": "no",
+#     "balance": 0,
+#     "housing": "no",
+#     "loan": "no",
+#     "contact": "cellular",
+#     "duration": 120,
+#     "campaign": 1,
+#     "pdays": -1,
+#     "previous": 0,
+#     "poutcome": "failure"
+#   }
+# ]
+# result=makePrediction(element)
+# print(result)
+# for r in result:
+#     print(r)
+
+
